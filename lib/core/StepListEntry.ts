5093bbcdd4f0482f98b87e4d0ab63ec0ec227c04
Pierre Romera <hello@pirhoo.com>
2025-01-03 16:55:53 +0100

    fix: make StepListEntry non abstract

--- lib/core/StepListEntry.ts
+++ lib/core/StepListEntry.ts
@@ -2,4 +2,8 @@ import type { StepList } from "../trotsky"
 import { Step } from "../trotsky"
 
 
-export abstract class StepListEntry<P = StepList, C = unknown, O = unknown> extends Step<P, C, O> { }
+export class StepListEntry<P = StepList, C = unknown, O = unknown> extends Step<P, C, O> { 
+  apply (): never {
+    throw new Error("Apply method not implemented.")
+  }
+}
