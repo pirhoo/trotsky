ece7dc979a86c0a4e35ac39999f26d1082e2d3b6
Pierre Romera <hello@pirhoo.com>
2025-01-05 00:55:12 +0100

    refactor: remove interface

--- lib/core/StepPostLike.ts
+++ lib/core/StepPostLike.ts
@@ -1,11 +1,6 @@
 import { Step, type StepPost, type StepPostOutput } from "../trotsky"
 
-export interface StepPostLikeRecordRef {
-  "uri": string;
-  "cid": string;  
-}
-
-export type StepPostLikeOutput = StepPostLikeRecordRef
+export type StepPostLikeOutput = { "uri": string; "cid": string }
 
 export class StepPostLike<P = StepPost, C extends StepPostOutput = StepPostOutput, O extends StepPostLikeOutput = StepPostLikeOutput> extends Step<P, C, O> {
   async apply () {
