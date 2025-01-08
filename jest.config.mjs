c3e9ccaf7ab5c076dbd83e7b5cbd544af633f350
Pierre Romera <hello@pirhoo.com>
2025-01-08 20:02:44 +0100

    test: increase test timeout

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,6 +3,8 @@ export default {
   transform: {
     '^.+\\.(t|j)sx?$': ['@swc/jest'],
   },
-  testTimeout: 15000,
+  // 30s is a lot of time for a test to run but we need to account for the time it takes 
+  // to start the TestServer provided by @atproto/dev-env
+  testTimeout: 30e3,
   setupFiles: ['<rootDir>/jest.setup.ts'],
 }
